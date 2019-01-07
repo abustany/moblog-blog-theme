8d9c697b62dd5626ff6a5fb54818612a0a03a8a4
Adrien Bustany <adrien@bustany.org>
2019-01-07 20:19:46 +0100

    Remove favicon, it's moved to the blog itself

--- generate-favicon.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-cd $(dirname $0)
-
-inkscape --without-gui -f favicon.svg --export-png=favicon_32.png --export-area-page -w 32 -h 32
-inkscape --without-gui -f favicon.svg --export-png=favicon_16.png --export-area-page -w 16 -h 16
-convert -background transparent favicon_16.png favicon_32.png static/favicon.ico
-rm favicon_16.png favicon_32.png
